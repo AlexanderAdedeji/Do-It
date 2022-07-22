bead2e4173e6be6a3dfd043480a81a7ff750a98a
Alexander <alexadedeji15@gmail.com>
2022-07-22 09:24:24 +0100

    changed notodo image to gif

--- src/components/NoTodoScreen.jsx
+++ src/components/NoTodoScreen.jsx
@@ -1,11 +1,11 @@
-import NoTodoImg from "../assets/notodo.svg";
+import NoTodoImg from "../assets/notodo.gif";
 import styles from '../styles/modules/notodo.module.scss'
 import React from "react";
 
 const NoTodo = ({ text, button, btnText, btnAction }) => {
   return (
     <div id={styles.no__file}>
-      <img src={NoTodoImg} alt="" width="200" />
+      <img src={NoTodoImg} alt="" width="300" />
       <p>{text}</p>
       {button && <button className="btn btn-success" onClick={() => btnAction()}>{btnText}</button>}
     </div>
